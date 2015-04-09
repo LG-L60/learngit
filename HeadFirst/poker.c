39f4dca6cab531dbc713841f32bffb2a973c9c65
lanhai <lan.hai@eastaeon.com>
2015-04-09 10:59:37 +0800

    card21:自己乱写

---
+++ HeadFirst/poker.c
@@ -0,0 +1,30 @@
+int main_count = 11;
+if (card_count > 10)
+  puts("这副牌的赢面很大，我要加注！）;
+int c = 10;
+while (c > 0){
+ puts("我决定不在课堂上写代码！");
+c = c - 1;
+}
+
+
+char ex[20];
+puts("输入男友的名字: ");
+scanf("%19s, ex");
+printf("亲爱的%s,我们分手吧。\n", ex);
+
+
+char suit = 'H';
+switc(suit){
+
+case'C':
+   puts("梅花(Clubs)");
+   break;
+case'D':
+   puts("方块(Diamonds)");
+   break;
+case'H':
+   puts("红心(Hearts)");
+default:
+   puts("黑桃(Spades)");
+}
